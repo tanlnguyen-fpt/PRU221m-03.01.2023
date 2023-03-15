7d109e050f0875ba5fcc272ed1f7d130ad7ebbbe
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2023-03-15 14:54:16 +0700

    first commit

---
+++ Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number: ");
+var input = Console.ReadLine();
+
+try
+{
+    int number = int.Parse(input);
+}
+catch (FormatException)
+{
+
+    Console.WriteLine("Format Exception!");
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Overflow Exception!");
+}
+finally
+{
+    Console.WriteLine("This is the final block");
+}
+
+
+
